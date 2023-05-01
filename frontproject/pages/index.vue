ba32cb0b37fa161d0c95d82ad977c1080a6b1c62
josesanfri <jose990.jsn@gmail.com>
2023-05-01 19:16:26 +0200

    change nuxt version

--- frontproject/pages/index.vue
+++ frontproject/pages/index.vue
@@ -1,43 +1,9 @@
 <template>
-    <article class="main">
-        <section class="main-about">
-            <hgroup class="main-about-title">
-                <h1>Mi pagina</h1>
-            </hgroup>
-            <NuxtLink to="/about">
-                About page
-            </NuxtLink>
-        </section>
-  </article>
+  <Tutorial/>
 </template>
 
-<script lang="ts">
+<script>
 export default {
-    layout: 'base'
+  name: 'IndexPage'
 }
 </script>
-
-<style lang="sass" scoped>
-    .main
-        min-height: calc(100vh - 74px)
-        display: grid
-        grid-template-columns: 1fr
-        @media screen and (min-width: 1024px)
-            grid-template-columns: repeat(2, 1fr)
-
-        &-about
-            padding: 5rem
-            background: #00000099
-            display: grid
-            grid-template-rows: auto
-            max-height: 900px
-            color: #fff
-            &-title
-                h1
-                    text-align: center
-                    font-size: 3rem
-                    font-weight: bold
-                    text-decoration: underline 
-                    @media screen and (min-width: 1024px)
-                        padding-bottom: 2rem
-</style>
